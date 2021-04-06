01b2aefc55f76eefeb23d1ce86c0907414242124
Адизбек Эргашев <adizbek1998@gmail.com>
2021-04-06 15:09:00 +0500

    Fix signature

--- library/src/main/java/com/github/adizbek/easydownload/library/helpers.kt
+++ library/src/main/java/com/github/adizbek/easydownload/library/helpers.kt
@@ -3,7 +3,10 @@ package com.github.adizbek.easydownload.library
 import java.io.InputStream
 import java.io.OutputStream
 
-fun InputStream.copyToWithProgress(out: OutputStream, onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Any): Long {
+fun InputStream.copyToWithProgress(
+    out: OutputStream,
+    onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Unit
+): Long {
     var bytesCopied: Long = 0
     val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
     var bytes = read(buffer)
