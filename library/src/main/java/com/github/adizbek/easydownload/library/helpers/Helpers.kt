9d0fd0e5c617c57efa07fbb830a9068de80cb2d3
Адизбек Эргашев <adizbek1998@gmail.com>
2021-04-13 14:03:58 +0500

    Improved upload progress

--- library/src/main/java/com/github/adizbek/easydownload/library/helpers/Helpers.kt
+++ library/src/main/java/com/github/adizbek/easydownload/library/helpers/Helpers.kt
@@ -3,18 +3,21 @@ package com.github.adizbek.easydownload.library.helpers
 import java.io.InputStream
 import java.io.OutputStream
 
+const val BUFFER_SIZE = 16 * 1028
+
 fun InputStream.copyToWithProgress(
     out: OutputStream,
-    onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Unit
+    onCopy: (totalBytesCopied: Long, bytesJustCopied: Int) -> Unit,
 ): Long {
     var bytesCopied: Long = 0
-    val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+    val buffer = ByteArray(BUFFER_SIZE)
     var bytes = read(buffer)
     while (bytes >= 0) {
         out.write(buffer, 0, bytes)
         bytesCopied += bytes
         onCopy(bytesCopied, bytes)
         bytes = read(buffer)
+        out.flush()
     }
     return bytesCopied
 }
